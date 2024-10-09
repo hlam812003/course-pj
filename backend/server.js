4d2268a88205f21d2969d9247b37e9ac42ac4393
HWAN <68631787+hlam812003@users.noreply.github.com>
2024-10-09 14:31:47 +0700

    .

--- backend/server.js
+++ backend/server.js
@@ -1,39 +1,31 @@
 import Fastify from 'fastify';
 import fastifyJwt from '@fastify/jwt';
-import mongoose from 'mongoose';
+import connectToDatabase from './db.js';
 import dotenv from 'dotenv';
 import adminRoutes from './routes/admin.js';
 
 dotenv.config();
 
-
 const fastify = Fastify({
   logger: true
-})
+});
 
-
-fastify.register(fastifyJwt, 
-{
-  secret: 'supersecret', 
+fastify.register(fastifyJwt, {
+  secret: process.env.JWT_SECRET,
 });
 
 fastify.register(adminRoutes);
 
-const main = async () => {
+const startServer = async () => {
   try {
-    mongoose.connect("mongodb+srv://admin-huyhoang:123@cluster0.e0cvr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/elearningData")
-    .then(() => {
-      console.log('MongoDB connected');
-    }).catch(err => {
-      console.error('MongoDB connection error:', err);
-    });
-
-    await fastify.listen({ port: 3000 });
-    console.log('Server is running on port 3000');
+    await connectToDatabase();
+    const port = process.env.PORT || 3000;
+    await fastify.listen({ port });
+    console.log(`Server is running on port ${port}`);
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
   }
-}
+};
 
-main();
+startServer();
